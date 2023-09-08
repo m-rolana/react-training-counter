dffe4005bcab9681793a344ff83bfdc5ed517a11
Maria Mikhalska <rolana93@gmail.com>
2023-09-08 17:48:19 +0300

    add eslint

--- src/components/CounterButton.js
+++ src/components/CounterButton.js
@@ -1,10 +1,11 @@
+import PropTypes from "prop-types";
 import Button from "@mui/material/Button";
 
 const style = {
   margin: 2,
   width: 150,
   height: 50,
-  fontSize: 36
+  fontSize: 36,
 };
 
 function CounterButton({ color, onClickFn, text }) {
@@ -20,5 +21,11 @@ function CounterButton({ color, onClickFn, text }) {
     </Button>
   );
 }
+
+CounterButton.propTypes = {
+  color: PropTypes.string.isRequired,
+  onClickFn: PropTypes.string.isRequired,
+  text: PropTypes.string,
+};
 
 export default CounterButton;
